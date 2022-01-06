49b5baca25c58515a1118608cd04ddc34937c93b
pedromachakio <pedromachakio@hotmail.com>
2022-01-06 18:36:05 -0100

    added some things left to see

---
+++ src/test/java/com/backend/projects/unittesting/MockingDemo/ThingsLeftToSee/ThingsLeftToSee.java
@@ -0,0 +1,13 @@
+package com.backend.projects.unittesting.MockingDemo.ThingsLeftToSee;
+
+// TODO
+public class ThingsLeftToSee {
+
+    // video 29 -> Json Assert, Strict True & Strict false (true tem que ser tudo igual, false tem que ter os mesmos valores mas pode nem ter as props todas do json
+    // JSONAssert.assertEquals(expectedResponse, actualResponse, Booleano);
+    // Ps. Json Assert doesnt even need escape characters
+
+
+
+
+}
